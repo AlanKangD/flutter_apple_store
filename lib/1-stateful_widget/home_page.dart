73ea6608def3761f15c01293acb85d7b9c78f191
alan <kno12350@gmail.com>
2024-04-14 13:22:31 +0900

    부모 위젯인 home_page 에 값을 저장하며 자식 위젯에 함수 및 데이터를 전달하며 중앙집권데이터 관리 형식으로 구현 되어 있습니다

--- lib/1-stateful_widget/home_page.dart
+++ lib/1-stateful_widget/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:apple_store/1-stateful_widget/card.dart';
 import 'package:apple_store/1-stateful_widget/store.dart';
 import 'package:apple_store/common/bottom_bar.dart';
+import 'package:apple_store/common/product.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,22 +15,42 @@ class _HomePageState extends State<HomePage> {
   /// 현재 선택된 index
   int currentIndex = 0;
 
+  // 카트에 담긴 상품 목록
+  List<Product> cartProductList = [];
+
+  // 상품 클릭
+  void onProductPressed(Product product) {
+    setState(() {
+      if (cartProductList.contains(product)) {
+        cartProductList.remove(product);
+      } else {
+        cartProductList.add(product);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
         index: currentIndex,
-        children: const [
+        children: [
           /// Store
-          Store(),
+          Store(
+            cartProductList: cartProductList,
+            onPressed: onProductPressed, // flutter 에서는 함수 이름만 작성해도 함수를 넘겨줄 수 있음
+          ),
 
           /// Cart
-          Cart(),
+          Cart(
+            cartProductList: cartProductList,
+            onPressed: onProductPressed, // flutter 에서는 함수 이름만 작성해도 함수를 넘겨줄 수 있음
+          ),
         ],
       ),
       bottomNavigationBar: BottomBar(
         currentIndex: currentIndex,
-        cartTotal: "0",
+        cartTotal: "${cartProductList.length}",   
         onTap: (index) => setState(() {
           currentIndex = index;
         }),
